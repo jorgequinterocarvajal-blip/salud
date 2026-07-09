5076d332eeff734f0c7cb6373a6d23b2090f3975
jorgequinterocarvajal-blip <jorge.quintero.carvajal@gmail.com>
2026-07-09 23:22:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='plan-salud-v2';
+const CACHE='plan-salud-v3';
 const FILES=['./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
